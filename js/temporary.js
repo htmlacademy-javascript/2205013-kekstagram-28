e1f13be56f8efac1eaf9f7041f575b1425ebb525
Aleksei Markin <markin.inbox@gmail.com>
2023-03-12 16:15:58 +0300

    Добавляет правки линтера

--- js/temporary.js
+++ js/temporary.js
@@ -1,29 +1,29 @@
 
-//Домашняя работа 7 раздел
+// //Домашняя работа 7 раздел
 
-import createImageStateList from './data.js';
+// import createImageStateList from './data.js';
 
-const picturesList = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('.picture');
-const pictureStateList = createImageStateList();
-const fragment = document.createDocumentFragment();
+// const picturesList = document.querySelector('.pictures');
+// const pictureTemplate = document.querySelector('#picture')
+//   .content
+//   .querySelector('.picture');
+// const pictureStateList = createImageStateList();
+// const fragment = document.createDocumentFragment();
 
-pictureStateList.forEach(picture => {
-  const newPicture = pictureTemplate.cloneNode(true);
+// pictureStateList.forEach(picture => {
+//   const newPicture = pictureTemplate.cloneNode(true);
 
-  newPicture.querySelector('.picture__img').src = picture.url;
-  newPicture.querySelector('.picture__img').alt = picture.description;
-  newPicture.querySelector('.picture__likes').textContent = picture.likes;
-  newPicture.querySelector('.picture__comments').textContent = picture.comments.length;
-  fragment.append(newPicture);
-});
+//   newPicture.querySelector('.picture__img').src = picture.url;
+//   newPicture.querySelector('.picture__img').alt = picture.description;
+//   newPicture.querySelector('.picture__likes').textContent = picture.likes;
+//   newPicture.querySelector('.picture__comments').textContent = picture.comments.length;
+//   fragment.append(newPicture);
+// });
 
-const renderThumbnails = () => {
-  picturesList.appendChild(fragment);
-};
+// const renderThumbnails = () => {
+//   picturesList.appendChild(fragment);
+// };
 
-export default renderThumbnails;
+// export default renderThumbnails;
 
 
