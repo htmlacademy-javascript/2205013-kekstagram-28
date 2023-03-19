4250d21437d20eb2c9c1eb42a9d97b64c7ce5701
Aleksei Markin <markin.inbox@gmail.com>
2023-03-19 20:08:30 +0300

    Добавляет onChange в разметку

--- js/form.js
+++ js/form.js
@@ -1,6 +1,11 @@
 const upLoad = document.querySelector('#upload-file');
 const upLoadForm = document.querySelector('.img-upload__overlay');
 
+const openUpLoad = () => {
+  upLoadForm.classList.remove('hidden');
+
+}
+
 upLoad.addEventListener('change', () => {
   // event.preventDefault();
   upLoadForm.classList.remove('hidden');
