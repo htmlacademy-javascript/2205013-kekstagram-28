fe0e0af2f6deb20e61b73df3f3cd1fcaac26379c
Aleksei Markin <markin.inbox@gmail.com>
2023-03-20 18:05:45 +0300

    Подключает Pristine в index

--- js/form.js
+++ js/form.js
@@ -1,12 +1,17 @@
 const upLoad = document.querySelector('#upload-file');
 const upLoadForm = document.querySelector('.img-upload__overlay');
-
-const openUpLoad = () => {
-  upLoadForm.classList.remove('hidden');
-
-}
+const cancelButton = document.querySelector('#upload-cancel');
 
 upLoad.addEventListener('change', () => {
   // event.preventDefault();
   upLoadForm.classList.remove('hidden');
 });
+
+
+cancelButton.addEventListener('click', () => {
+  upLoadForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+});
+
+
