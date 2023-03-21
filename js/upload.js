167e5517ec7fcb69138f4f6682c6cbaf55949c7d
Aleksei Markin <markin.inbox@gmail.com>
2023-03-21 17:50:59 +0300

    Добавляет открытие модального окна после добавления нового фото

--- js/upload.js
+++ js/upload.js
@@ -1,1 +1,20 @@
+import openPopup from './popup.js';
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.querySelector('.img-upload__form');
+/**
+ * @type {HTMLDivElement}
+ */
+const popup = form.querySelector('.img-upload__overlay');
 
+/**
+ * @param {event} event
+ */
+const onFormChange = (event) => {
+  if (event.target === form.elements.filename) {
+    openPopup(popup);
+  }
+};
+
+form.addEventListener('change', onFormChange);
