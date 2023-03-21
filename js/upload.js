71512fa90feec791a03bd5190ed1251c91650980
Aleksei Markin <markin.inbox@gmail.com>
2023-03-21 18:09:34 +0300

    Подключает Pristine и добавляет основные настройки

--- js/upload.js
+++ js/upload.js
@@ -8,13 +8,22 @@ const form = document.querySelector('.img-upload__form');
  */
 const popup = form.querySelector('.img-upload__overlay');
 
+// @ts-ignore
+const pristine = new Pristine(form, {
+  classTo: 'form-group',
+  errorTextParent: 'img-upload__field-wrapper',
+});
+
 /**
  * @param {event} event
  */
 const onFormChange = (event) => {
   if (event.target === form.elements.filename) {
+    //TODO изображение
     openPopup(popup);
   }
 };
 
 form.addEventListener('change', onFormChange);
+
+openPopup(popup);
