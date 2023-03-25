505acd3b3f2bb58c72d3de9a97b1be97cd9ec677
Aleksei Markin <markin.inbox@gmail.com>
2023-03-25 12:43:45 +0300

    Добавляет обработчик на кнопки упарвления масштабом

--- js/upload-preview.js
+++ js/upload-preview.js
@@ -20,7 +20,23 @@ const scaleControl = document.querySelector('.img-upload__scale');
 const setScale = (percent) => {
   picture.style.setProperty('transform', `scale(${percent / 100})`);
   scaleControl.querySelector('input').setAttribute('value', `${percent}%`);
+};
 
+/**
+ * @param {MouseEvent} event
+ */
+const onScaleControlClick = (event) => {
+  const [less, input, more] = scaleControl.querySelectorAll('input, button');
+  const value = Number.parseFloat(input.getAttribute('value'));
+
+  switch (event.target) {
+    case less:
+      setScale(Math.max(value - Scale.STEP, Scale.MIN));
+      break;
+    case more:
+      setScale(Math.min(value + Scale.STEP, Scale.MAX));
+      break;
+  }
 };
 
 /**
@@ -30,6 +46,7 @@ const updatePreview = (data) => {
   //TODO подстановка изображения
   void data
   setScale(Scale.MAX);
+  scaleControl.addEventListener('click', onScaleControlClick);
 };
 
 export default updatePreview;
