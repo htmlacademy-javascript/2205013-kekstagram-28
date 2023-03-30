6e80e4a71746c6198ceac6ff8dccd31ccda2169d
Aleksei Markin <markin.inbox@gmail.com>
2023-03-30 18:13:25 +0300

    Добавляет закрытие инфоокна по клику и нажатию ESK

--- js/status-popup.js
+++ js/status-popup.js
@@ -1,4 +1,30 @@
 /**
+ * @param {KeyboardEvent} event
+ */
+const onDocumentKeydown = (event) => {
+  // const isEscapeKey = event.key.startsWith('Esc');
+  if(event.key.startsWith('Esc')) {
+    /**
+     * @type {HTMLElement}
+     */
+    const popup = document.querySelector('.success, .error');
+
+    popup.click();
+  }
+};
+
+/**
+ * @param {MouseEvent & {target: Element, currentTarget: HTMLElement}} event
+ */
+const onPopupClick = (event) => {
+  if (event.target.matches('section, button')) {
+    event.currentTarget.remove();
+
+    document.removeEventListener('keydown', onDocumentKeydown);
+  }
+};
+
+/**
  * @param {'success'|'error'} type
  */
 const openStatusPopup = (type) => {
@@ -7,10 +33,14 @@ const openStatusPopup = (type) => {
    */
   const popupTemplate = document.querySelector(`#${type}`);
   const popup =
-  /**
-   * @type {HTMLElement}
-   */
-  (popupTemplate.content.querySelector(`.${type}`).cloneNode(true));
+    /**
+     * @type {HTMLElement}
+     */
+    (popupTemplate.content.querySelector(`.${type}`).cloneNode(true));
+
+  popup.addEventListener('click', onPopupClick);
+  document.addEventListener('keydown', onDocumentKeydown);
+
   document.body.append(popup);
 };
 export default openStatusPopup;
